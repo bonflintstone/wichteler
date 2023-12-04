8e68a504711dba1c368f3955a6b5d2f0bd96a42c
Bonflintstone <freddy@railslove.com>
2023-12-04 19:45:38 +0100

    Fix bug

--- app/services/create_game_service.rb
+++ app/services/create_game_service.rb
@@ -4,7 +4,7 @@ class CreateGameService
 
     names = params[:participant_names].split(",").map(&:strip).shuffle
 
-    participants = names.map { participants.build(name: _1) }
+    participants = names.map { Participant.build(name: _1, game:) }
     participants.each_with_index { |participant, i| participant.receiver = participants[(i + 1) % participants.length] }
 
     game.participants = participants.shuffle
